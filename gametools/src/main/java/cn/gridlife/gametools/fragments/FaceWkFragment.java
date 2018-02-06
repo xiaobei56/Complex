9b84753a6fa0b3efcd218c854778d550ce009e96
xiaobei56 <xiaobei_63@163.com>
2018-02-07 00:48:50 +0800

    submit

--- gametools/src/main/java/cn/gridlife/gametools/fragments/FaceWkFragment.java
+++ gametools/src/main/java/cn/gridlife/gametools/fragments/FaceWkFragment.java
@@ -1,6 +1,7 @@
 package cn.gridlife.gametools.fragments;
 
 import android.app.Fragment;
+import android.view.View;
 
 import cn.gridlife.generallibrary.fragments.BFragment;
 
@@ -13,8 +14,51 @@ import cn.gridlife.generallibrary.fragments.BFragment;
 
 public class FaceWkFragment extends BFragment {
 
+    /**
+     * 第一次界面用户可见: 初始化View的数据
+     */
+    @Override
+    protected void onFirstUserVisible() {
+
+    }
+
+    /**
+     * 当用户可见时
+     */
+    @Override
+    protected void onUserVisible() {
+
+    }
+
+    /**
+     * 当用户不可见时
+     */
+    @Override
+    protected void onUserInvisible() {
+
+    }
+
+    /**
+     * 初始化View 及 Event
+     * onViewCreated
+     *
+     * @param view 布局View
+     */
+    @Override
+    protected void initViewsAndEvents(View view) {
+
+    }
+
     @Override
     protected int getContentViewLayoutID() {
         return 0;
+    }
+
+    /**
+     * OnDestroy do something
+     */
+    @Override
+    protected void DetoryViewAndThing() {
+
     }
 }
